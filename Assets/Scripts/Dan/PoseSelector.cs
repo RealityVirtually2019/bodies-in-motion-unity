48d658f69ab642d49fc7ccec465d9166429378c5
Jiexun Xu <jxx1311@yahoo.com>
2019-01-20 11:36:44 -0500

    Animated pose seletor

--- Assets/Scripts/Dan/PoseSelector.cs
+++ Assets/Scripts/Dan/PoseSelector.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PoseSelector : MonoBehaviour
 {
     public GameObject[] poseThings;
-
-    private int activePose = 0;
+    public float poseChangeInteral = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,20 +21,70 @@ public class PoseSelector : MonoBehaviour
         }
     }
 
-    public void SetPose(int poseNum)
+    float lastCallTime = -10;
+    int ctr = 0;
+    private void Update()
     {
-        if (poseNum <= poseThings.Length-1)
+        if (Time.time - lastCallTime > poseChangeInteral)
         {
-            
-            poseThings[activePose].SetActive(false);
-
-            activePose = poseNum;
-            poseThings[poseNum].SetActive(true);
+            SetPose(ctr);
+            ctr++;
+            ctr = ctr % 4;
+            lastCallTime = Time.time;
         }
     }
 
-    public GameObject GetActivePose()
+    public void SetPose(int poseNum)
     {
-        return poseThings[activePose];
+        if (poseNum <= poseThings.Length - 1)
+        {
+            for (int i = 0; i < poseThings.Length; i++)
+            {
+                // poseThings[i].SetActive(false);
+                if (i != poseNum)
+                    StartCoroutine(unselectPoseThing(i, 3));
+            }
+
+            // poseThings[poseNum].SetActive(true);
+            StartCoroutine(selectPoseThing(poseNum, 3));
+        }
+    }
+
+    private IEnumerator selectPoseThing(int poseNum, float v)
+    {
+        poseThings[poseNum].SetActive(true);
+        Vector3 pos = poseThings[poseNum].transform.position;
+        pos.y = -3f;
+        poseThings[poseNum].transform.position = pos;
+        float step = (v / 5.88f) * Time.fixedDeltaTime;
+        float t = 0;
+        while (t <= 1.0f)
+        {
+            t += step;
+            pos.y = pos.y + v * Time.deltaTime;
+            poseThings[poseNum].transform.position = pos;
+            yield return new WaitForFixedUpdate();
+        }
+        pos.y = 2.88f;
+        poseThings[poseNum].transform.position = pos;
+    }
+
+    private IEnumerator unselectPoseThing(int poseNum, float v)
+    {
+        Vector3 pos = poseThings[poseNum].transform.position;
+        pos.y = 2.88f;
+        poseThings[poseNum].transform.position = pos;
+        float step = (v / 5.88f) * Time.fixedDeltaTime;
+        float t = 0;
+        while (t <= 1.0f)
+        {
+            t += step;
+            pos.y = pos.y - v * Time.deltaTime;
+            poseThings[poseNum].transform.position = pos;
+            yield return new WaitForFixedUpdate();
+        }
+        pos.y = -3f;
+        poseThings[poseNum].transform.position = pos;
+        poseThings[poseNum].SetActive(false);
     }
 }
