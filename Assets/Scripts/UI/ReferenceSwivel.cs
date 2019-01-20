bd923c3210644b6fc7eebe6d0b4d093fb959f640
Daniel Bryand <daniel.bryand@gmail.com>
2019-01-20 11:48:54 -0500

    Fixed the rotation on start

--- Assets/Scripts/UI/ReferenceSwivel.cs
+++ Assets/Scripts/UI/ReferenceSwivel.cs
@@ -9,12 +9,21 @@ public class ReferenceSwivel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(StartDelay());
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(0.2f);
+
         Vector3 rot = camera.rotation.eulerAngles;
         rot.x = 0;
         rot.z = 0;
         Quaternion quatRot = new Quaternion();
         quatRot.eulerAngles = rot;
         transform.rotation = quatRot;
+
+        //transform.position = camera.transform.position;
     }
 
     // Update is called once per frame
