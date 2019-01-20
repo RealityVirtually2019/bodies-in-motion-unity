3dffe2b56e6fbd5275aeea23344188e78a8ec857
Jiexun Xu <jxx1311@yahoo.com>
2019-01-20 01:40:16 -0500

    Added reference model prefabs by Max. Now the light intensity and the fire size at the fire place change depending on how you perform.

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Utilities
 {
-    public static float SimilarityScore(GameObject[] obj1, GameObject[] obj2)
+    public static float SimilarityScore(Transform[] obj1, Transform[] obj2)
     {
         if (obj1.Length != obj2.Length)
         {
@@ -14,8 +14,11 @@ public class Utilities
         float score = 0;
         for(int i = 0; i < obj1.Length; i++)
         {
-            Vector3 dif = obj1[i].transform.position - obj2[i].transform.position;
-            score += Mathf.Sqrt(dif[0] * dif[0] + dif[1] * dif[1] + dif[2] * dif[2]);
+            if (obj1[i] != null && obj2[i] != null)
+            {
+                Vector3 dif = obj1[i].position - obj2[i].position;
+                score += Mathf.Sqrt(dif[0] * dif[0] + dif[1] * dif[1] + dif[2] * dif[2]);
+            }
         }
         return score;
     }
